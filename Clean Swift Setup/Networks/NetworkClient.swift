b13bc72881f8c25b32ab49da164b18c1cc263fe4
Lucas Abijmil <lucas.abijmil@gmail.com>
2021-12-05 17:45:18 +0100

    Remove async fetch with Result

--- Clean Swift Setup/Networks/NetworkClient.swift
+++ Clean Swift Setup/Networks/NetworkClient.swift
@@ -5,44 +5,37 @@
 import Foundation
 
 protocol NetworkClient {
-  func requestData(route: ApiRoute) async throws -> Data
-  func requestResult(route: ApiRoute) async -> Result<Data, Error>
+  func request(route: ApiRoute) async throws -> Data
   func request(route: ApiRoute, completion: @escaping(Result<Data, Error>) -> Void)
   
-  func requestData(with url: URL) async throws -> Data
-  func requestResult(with url: URL) async -> Result<Data, Error>
+  func request(with url: URL) async throws -> Data
   func request(with url: URL, completion: @escaping (Result<Data, Error>) -> Void)
 }
 
 final class DefaultNetworkClient: NetworkClient {
-
+  
   private enum HTTPError: Error {
     case badReponse
     case noData
   }
-
+  
   private let session: URLSession
-
+  
   init(session: URLSession) {
     self.session = session
   }
   
-  func requestData(route: ApiRoute) async throws -> Data {
+  func request(route: ApiRoute) async throws -> Data {
     let url = URLBuilder.url(for: route)
-    return try await requestData(with: url)
+    return try await request(with: url)
   }
   
-  func requestResult(route: ApiRoute) async -> Result<Data, Error> {
-    let url = URLBuilder.url(for: route)
-    return await requestResult(with: url)
-  }
-
   func request(route: ApiRoute, completion: @escaping(Result<Data, Error>) -> Void) {
     let url = URLBuilder.url(for: route)
     request(with: url, completion: completion)
   }
   
-  func requestData(with url: URL) async throws -> Data {
+  func request(with url: URL) async throws -> Data {
     let (data, response) = try await session.data(from: url)
     guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
       throw HTTPError.badReponse
@@ -50,18 +43,6 @@ final class DefaultNetworkClient: NetworkClient {
     return data
   }
   
-  func requestResult(with url: URL) async -> Result<Data, Error> {
-    do {
-      let (data, response) = try await session.data(from: url)
-      guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-        return .failure(HTTPError.badReponse)
-      }
-      return .success(data)
-    } catch {
-      return .failure(error)
-    }
-  }
-
   func request(with url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
     session.dataTask(with: url) { data, response, error in
       if let error = error {
